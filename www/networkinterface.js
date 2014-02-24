c61ef778a8fa5fd9b3c63ae7fe42c30d789ae849
Samer Albahra <salbahra@gmail.com>
2014-02-24 17:51:52 -0600

    Update networkinterface.js

--- www/networkinterface.js
+++ www/networkinterface.js
@@ -3,7 +3,7 @@ var exec = require('cordova/exec');
 var networkinterface = function() {
 };
 
-Keyboard.getIPAddress = function(success, fail) {
+networkinterface.getIPAddress = function(success, fail) {
     exec(success, fail, "networkinterface", "getIPAddress", []);
 };
 
