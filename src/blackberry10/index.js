3eaff4f4f178ce724444b4e3b007920815e92576
Samer Albahra <salbahra@gmail.com>
2014-04-19 16:50:08 -0500

    Added BlackBerry 10 Support

---
+++ src/blackberry10/index.js
@@ -0,0 +1,24 @@
+function getIP() {
+    var con;
+
+    try {
+        con = window.qnx.webplatform.device.activeConnection;
+    }
+    catch (e) { return false; }
+
+    if (!con || con.type == 'cellular') return false;
+    return con.defaultGateways[0];
+}
+
+module.exports = {
+    getIPAddress: function (success, fail, args, env) {
+        var result = new PluginResult(args, env),
+            ip = getIP();
+
+        if (!ip) {
+            result.error();
+        } else {
+            result.ok(ip);
+        }
+    }
+};
