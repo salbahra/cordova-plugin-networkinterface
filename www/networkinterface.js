ea95de0ce0f66429c37c941ff2c30df16822a8b1
Samer Albahra <salbahra@gmail.com>
2018-03-28 18:21:57 -0500

    Add method to retrieve the wifi or carrier (fallback) IP address

--- www/networkinterface.js
+++ www/networkinterface.js
@@ -9,6 +9,10 @@ networkinterface.getCarrierIPAddress = function( success, fail ) {
     cordova.exec( success, fail, "networkinterface", "getCarrierIPAddress", [] );
 };
 
+networkinterface.getIPAddress = function( success, fail ) {
+    cordova.exec( success, networkinterface.getCarrierIPAddress.bind( null, success, fail ), "networkinterface", "getWiFiIPAddress", [] );
+};
+
 networkinterface.getHttpProxyInformation = function(url, success, fail ) {
     cordova.exec( success, fail, "networkinterface", "getHttpProxyInformation", [url] );
 };
